799c8171df3322e371679992c3031cdfd315b12d
tanya_bond <bondarenkotatiana96@gmail.com>
2021-07-09 13:35:20 -0700

    animated hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function() {
+  console.log('open menu');
+  header.classList.toggle('open'); //opening hambburger menu
+
+  if (header.classList.contains('open')) {
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
+  } else {
+    overlay.classList.add('fade-out');
+    overlay.classList.remove('fade-in');
+  }
+  
+});
