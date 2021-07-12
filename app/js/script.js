eb45abfbdf76c398c876b413c45b74290163a52a
tanya_bond <bondarenkotatiana96@gmail.com>
2021-07-12 15:18:13 -0700

    adding animeted mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,24 @@
+const body = document.querySelector('body');
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
   console.log('open menu');
-  header.classList.toggle('open'); //opening hambburger menu
+  header.classList.toggle('open'); //opening hamburger menu
+  body.classList.toggle('noscroll');
 
   if (header.classList.contains('open')) {
-    overlay.classList.add('fade-in');
-    overlay.classList.remove('fade-out');
+    fadeElems.forEach(el => {
+      el.classList.add('fade-in');
+      el.classList.remove('fade-out');
+    });
   } else {
-    overlay.classList.add('fade-out');
-    overlay.classList.remove('fade-in');
+    fadeElems.forEach(el => {
+      el.classList.remove('fade-in');
+      el.classList.add('fade-out');
+    });
   }
   
 });
